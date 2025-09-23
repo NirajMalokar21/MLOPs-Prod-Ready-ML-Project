da4eef65bd74fe5a5316e534d14c57a74b76f378
NirajMalokar21 <nirajmalokar@gmail.com>
2025-09-23 13:43:37 -0400

    Add requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops_project",
+    version="0.0.0",
+    author="Niraj Malokar",
+    author_email = "nirajmalokar@gmail.com",
+    packages=find_packages(),
+)
