d55f4b1af1c6b4a658d7e261e4d1f53c3f444c9a
NirajMalokar21 <nirajmalokar@gmail.com>
2025-10-16 12:07:38 -0400

    Set up AWS and add model evaluation and pusher

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -57,3 +57,18 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2025-niraj"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+REGION_NAME = "us-east-2"
+AWS_ACCESS_KEY_ID_ENV_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY_ENV_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8000
